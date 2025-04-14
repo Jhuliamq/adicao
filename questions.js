3a15a010a1ff6cd28ccb24d531c2650bc64838c3
Jhulia <127256522+Jhuliamq@users.noreply.github.com>
2025-04-14 08:17:55 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,93 @@
+const questao = [
+    {
+      question: "Quanto é 1 + 1 ?",
+      answers: [
+        { option: "5 ", correct: false},
+        { option: "3 ", correct: false},
+        { option: "1", correct: false},
+        { option: "2", correct: true},
+      ],
+    },
+    {
+      question: "Quanto é 3 + 1?",
+      answers: [
+        { option: "4", correct: true},
+        { option: "2", correct: false} ,
+        { option: "5 ", correct: false},
+        { option: "0", correct: false},
+      ],
+    },
+    {
+      question: "Quanto é 4 + 2?",
+      answers: [
+        { option: "8", correct: false },
+        { option: "5", correct: false },
+        { option: "6", correct: true },
+        { option: "3", correct: false },
+      ],
+    },
+    {
+      question: "Quanto é 7 + 1 ?",
+      answers: [
+        { option: "9", correct: false },
+        { option: "7", correct: false },
+        { option: "8", correct: true },
+        { option: "6", correct: false },
+      ],
+    },
+    {
+      question: "Quanto é 5 + 2?",
+      answers: [
+        { option: "8", correct: false },
+        { option: "7", correct: true },
+        { option: "10", correct: false },
+        { option: "9", correct: false },
+      ],
+    },
+    {
+      question: "Quanto é 6 + 3?",
+      answers: [
+        { option: "1", correct: false },
+        { option: "10", correct: false },
+        { option: "8", correct: false },
+        { option: "9", correct: true },
+      ],
+    },
+    {
+      question: "Quanto é 8 + 2?",
+      answers: [
+        { option: "9", correct: false },
+        { option: "10", correct: true },
+        { option: "11", correct: false },
+        { option: "12", correct: false },
+      ],
+    },
+    {
+      question: "Quanto é a soma de 7 + 1?",
+      answers: [
+        { option: "5", correct: false},
+        { option: "8", correct: true},
+        { option: "9", correct: false},
+        { option: "3", correct: false},
+      ],
+    },
+    
+    {
+      question: "Quanto é 3 + 5 ?",
+      answers: [
+        { option: "1 ", correct: false},
+        { option: "7 ", correct: false},
+        { option: "8 ", correct: true},
+        { option: "11 ", correct: false},
+      ],
+    },
+    {
+      question:"Quanto é 7 + 0 ?",
+      answers: [
+        { option:"7", correct: true},
+        { option:"5", correct: false},
+        { option:"0", correct: false},
+        { option:"70", correct: false},
+      ],
+    },
+  ];
